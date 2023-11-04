705ed8b7a160c1242fbc5fcad32079e3ab309ab5
Alex Jackson <contact@ajaxbits.com>
2023-11-04 17:51:59 -0500

    fix: proper image name in config.yaml

--- valetudopng/rootfs/docker_entrypoint.sh
+++ valetudopng/rootfs/docker_entrypoint.sh
@@ -20,6 +20,18 @@ if ! bashio::fs.file_exists "$VALETUDOPNG_DATA/config.yaml"; then
     bashio::exit.nok "Could not find $VALETUDOPNG_DATA/config.yaml. Please ensure it exists"
 fi
 
+# TODO: expose all config in frontend. Can detect mosquitto automatically with the below
+#
+# if bashio::config.is_empty 'mqtt' && bashio::var.has_value "$(bashio::services 'mqtt')"; then
+#     if bashio::var.true "$(bashio::services 'mqtt' 'ssl')"; then
+#         export ZIGBEE2MQTT_CONFIG_MQTT_SERVER="mqtts://$(bashio::services 'mqtt' 'host'):$(bashio::services 'mqtt' 'port')"
+#     else
+#         export ZIGBEE2MQTT_CONFIG_MQTT_SERVER="mqtt://$(bashio::services 'mqtt' 'host'):$(bashio::services 'mqtt' 'port')"
+#     fi
+#     export ZIGBEE2MQTT_CONFIG_MQTT_USER="$(bashio::services 'mqtt' 'username')"
+#     export ZIGBEE2MQTT_CONFIG_MQTT_PASSWORD="$(bashio::services 'mqtt' 'password')"
+# fi
+
 bashio::log.info "Starting ValetudoPNG..."
 cd /app
 exec valetudopng
