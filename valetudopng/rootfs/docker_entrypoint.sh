ec107b9428ec4ca63de2c29f9a3ace9416d44870
Alex Jackson <contact@ajaxbits.com>
2023-11-04 17:26:27 -0500

    chore: log file typos

--- valetudopng/rootfs/docker_entrypoint.sh
+++ valetudopng/rootfs/docker_entrypoint.sh
@@ -13,13 +13,13 @@ export VALETUDOPNG_DATA="$(bashio::config 'data_path')"
 
 if ! bashio::fs.directory_exists $VALETUDOPNG_DATA; then
     mkdir -p $VALETUDOPNG_DATA \
-        || bashio::exit.nok 'Failed to create initial AppDaemon configuration'
+        || bashio::exit.nok 'Failed to create initial ValetudoPNG configuration'
 fi
 
 if ! bashio::fs.file_exists "$VALETUDOPNG_DATA/config.yaml"; then
     bashio::exit.nok "Could not find $VALETUDOPNG_DATA/config.yaml. Please ensure it exists"
 fi
 
-bashio::log.info "Starting valetudopng..."
+bashio::log.info "Starting ValetudoPNG..."
 cd /app
 exec valetudopng
